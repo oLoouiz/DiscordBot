fc9aa8467cbec7f18dcc17dcf4a9b0ddb90d5776
looui <loouiz.felipe@hotmail.com>
2023-05-27 23:26:26 -0300

    Bot funcionando

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import cv2
 
 load_dotenv()
 
-GCP_PROJECT_ID = os.getenv('GCP_PROJECT_ID')
+#GCP_PROJECT_ID = os.getenv('GCP_PROJECT_ID')
 idCanal = os.getenv('idCanal')
 idBot = os.getenv("idBot")
 
@@ -23,39 +23,43 @@ async def on_ready():
 @client.event
 async def on_message(message):
 
-    #inicio bot ia identificador
 
-    a_id = message.author.id
-    if (a_id != idBot):
-        if message.content.startswith('/identifique'):  #precisaria colocar /identifique junto com a imagen
-   
-            for x in message.attachments:
-                print("attachment-->",x.url)
-                d_url = requests.get(x.url)
-                file_name = x.url.split('/')[-1]
-                with open(file_name, "wb") as f:
-                    f.write(d_url.content)
-
-            loaded_model = tf.keras.saving.load_model("model_ants-bees_V4.h5")
-            image_size = (256, 256)
-
-            imagem = cv2.imread(file_name)
-            imagem = cv2.resize(imagem, image_size)
-            imagem = imagem.reshape((1,256,256,3))
-            imagem = tf.cast(imagem/255. ,tf.float32)
-
-            if(loaded_model.predict(imagem) >= 0.5):
-                await message.channel.send('É uma abelha 🐝')
-            else:
-                await message.channel.send('É uma formiga 🐜')
-            os.remove(file_name)
-#fim 
 
     print("message-->", message)
     if message.author == client.user:
         return
 
-    if message.channel.id == idCanal:
+    if message.channel.id == 1097685290228908072:
+
+        # inicio bot ia identificador
+
+        a_id = message.author.id
+        if (a_id != idBot):
+            if message.content.startswith('/identifique'):  # precisaria colocar /identifique junto com a imagen
+
+                for x in message.attachments:
+                    print("attachment-->", x.url)
+                    d_url = requests.get(x.url)
+                    file_name = x.url.split('/')[-1]
+                    with open(file_name, "wb") as f:
+                        f.write(d_url.content)
+
+                loaded_model = tf.keras.saving.load_model("model_ants-bees_V4.h5")
+                image_size = (256, 256)
+
+                imagem = cv2.imread(file_name)
+                imagem = cv2.resize(imagem, image_size)
+                imagem = imagem.reshape((1, 256, 256, 3))
+                imagem = tf.cast(imagem / 255., tf.float32)
+
+                if (loaded_model.predict(imagem) >= 0.5):
+                    await message.channel.send('É uma abelha 🐝')
+                else:
+                    await message.channel.send('É uma formiga 🐜')
+                os.remove(file_name)
+        # fim
+
+
         if message.content.startswith('tchau'):
             await message.channel.send('Tchau!')
 
@@ -66,7 +70,7 @@ async def on_message(message):
         await message.channel.send("Apagando todas as mensagens do canal...")
         await message.channel.purge(limit=None)
 
-client.run("token do bot")
+client.run(os.getenv('token'))
 
 
 
